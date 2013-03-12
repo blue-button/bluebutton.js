33bb13c52dcd85cda1d0ae15819776b6b14b67b8
Tom Black <blacktm@gmail.com>
2013-03-12 16:28:19 -0400

    Adding 'Codes' module
    Used to manage coding systems throughout BB data.

---
+++ src/codes.js
@@ -0,0 +1,22 @@
+// codes.js
+
+var Codes = function() {
+
+  // dependancies
+  
+  // properties
+  var urls = {
+    snomed: 'http://purl.bioontology.org/ontology/snomedct/',
+    loinc: 'http://purl.bioontology.org/ontology/lnc/',
+    rxnorm: 'http://purl.bioontology.org/ontology/rxnorm/'
+  };
+  
+  // private methods
+  
+  // public methods
+  
+  // init
+  
+  return {
+  }
+}();
