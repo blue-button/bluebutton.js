97f1f17c99bc90927ca93ec35e365c0488cf829b
Tom Black <blacktm@gmail.com>
2013-04-08 23:30:03 -0400

    VA C32 addresses and given names are now arrays

--- src/sections/demographics.js
+++ src/sections/demographics.js
@@ -168,7 +168,6 @@ var Demographics = function () {
     el = patient.tag('providerOrganization');
     data.provider_organization = el.tag('name').val();
     data.provider_phone = el.tag('telecom').attr('value');
-    data.provider_street = el.tag('streetAddressLine').val();
     
     els = el.elsByTag('streetAddressLine');
     data.provider_street = [];
@@ -190,10 +189,16 @@ var Demographics = function () {
     
     el = xmlDOM.template('1.3.6.1.4.1.19376.1.5.3.1.1.1');
     patient = el.tag('patientRole');
-    
     el = patient.tag('patient').tag('name');
-    data.prefix = null;
-    data.given = el.tag('given').val();
+    data.prefix = el.tag('prefix').val();
+    
+    els = el.elsByTag('given');
+    data.given = [];
+    
+    for (var i = 0; i < els.length; i++) {
+      data.given.push(els[i].val());
+    }
+    
     data.family = el.tag('family').val();
     
     el = patient.tag('patient');
@@ -202,7 +207,13 @@ var Demographics = function () {
     data.marital_status = el.tag('maritalStatusCode').attr('displayName');
     
     el = patient.tag('addr');
-    data.street = el.tag('streetAddressLine').val();
+    els = el.elsByTag('streetAddressLine');
+    data.street = [];
+    
+    for (var i = 0; i < els.length; i++) {
+      data.street.push(els[i].val());
+    }
+    
     data.city = el.tag('city').val();
     data.state = el.tag('state').val();
     data.zip = el.tag('postalCode').val();
@@ -229,11 +240,25 @@ var Demographics = function () {
     data.guardian_relationship = el.tag('code').attr('displayName');
     data.guardian_home = el.tag('telecom').attr('value');
     el = el.tag('guardianPerson');
-    data.guardian_given = el.tag('given').val();
+    
+    els = el.elsByTag('given');
+    data.guardian_given = [];
+    
+    for (var i = 0; i < els.length; i++) {
+      data.guardian_given.push(els[i].val());
+    }
+    
     data.guardian_family = el.tag('family').val();
     
     el = patient.tag('guardian').tag('addr');
-    data.guardian_street = el.tag('streetAddressLine').val();
+    
+    els = el.elsByTag('streetAddressLine');
+    data.guardian_street = [];
+    
+    for (var i = 0; i < els.length; i++) {
+      data.guardian_street.push(els[i].val());
+    }
+    
     data.guardian_city = el.tag('city').val();
     data.guardian_state = el.tag('state').val();
     data.guardian_zip = el.tag('postalCode').val();
@@ -242,7 +267,14 @@ var Demographics = function () {
     el = patient.tag('providerOrganization');
     data.provider_organization = el.tag('name').val();
     data.provider_phone = el.tag('telecom').attr('value');
-    data.provider_street = el.tag('streetAddressLine').val();
+    
+    els = el.elsByTag('streetAddressLine');
+    data.provider_street = [];
+    
+    for (var i = 0; i < els.length; i++) {
+      data.provider_street.push(els[i].val());
+    }
+    
     data.provider_city = el.tag('city').val();
     data.provider_state = el.tag('state').val();
     data.provider_zip = el.tag('postalCode').val();
