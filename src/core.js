4882d01f7aac49dbf59422f28f599a9a5118e4ba
Tom Black <blacktm@gmail.com>
2013-07-07 23:53:25 -0400

    Major cleanup of `Core` module and added documentation
    Experimenting with inline documentation styles.

--- src/core.js
+++ src/core.js
@@ -1,53 +1,166 @@
-// core.js - Essential shared functionality
+/*
+ * core.js - Essential and shared functionality.
+ */
 
 var Core = function () {
+  
+  // Properties
+  ///////////////////////////
+  
+  // Private Methods
+  ///////////////////////////
 
-  // Establish the root object, `window` in the browser, or `global` in Node.
-  var root = this,
-      jsdom = undefined,
-      isNode = false,
-      doc = root.document; // Will be `undefined` if we're in Node
-
-  // Check if we're in Node. If so, pull in jsdom so we can
-  // simulate the DOM
-  if (typeof exports !== 'undefined') {
-    if (typeof module !== 'undefined' && module.exports) {
-      isNode = true;
-      jsdom = require("jsdom");
-      doc = new (jsdom.level(1, "core").Document)();
+  /*
+   * A function used to wrap DOM elements in an object so methods can be added
+   * to the element object. IE8 does not allow methods to be added directly to
+   * DOM objects.
+   */
+  var wrapElement = function (el) {
+    function wrapElementHelper(currentEl) {
+      return {
+        el: currentEl,
+        template: template,
+        tag: tag,
+        elsByTag: elsByTag,
+        attr: attr,
+        val: val,
+        isEmpty: isEmpty
+      }
     }
-  }
-
-  // properties
-  var ElementWrapper = function (el) {
-    return {
-      el: el,
-      template: template,
-      tag: tag,
-      elsByTag: elsByTag,
-      attr: attr,
-      val: val,
-      isEmpty: isEmpty
+    
+    // el is an array of elements
+    if (el.length) {
+      var els = [];
+      for (var i = 0; i < el.length; i++) {
+        els.push(wrapElementHelper(el[i]));
+      }
+      return els;
+    
+    // el is a single element
+    } else {
+      return wrapElementHelper(el);
     }
   };
   
-  // methods
+  /*
+   * Find element by tag name, then attribute value.
+   */
+  var tagAttrVal = function (el, tag, attr, value) {
+    el = el.getElementsByTagName(tag);
+    for (var i = 0; i < el.length; i++) {
+      // Workaround a bug in jsdom https://github.com/tmpvar/jsdom/issues/651
+      attr = isNode ? attr.toLowerCase() : attr;
+      if (el[i].getAttribute(attr) === value) {
+        return el[i];
+      }
+    }
+  };
   
-  // Cross-browser XML parsing
+  /*
+   * Search for a template ID, and return its parent element.
+   * Example:
+   *   <templateId root="2.16.840.1.113883.10.20.22.2.17"/>
+   * Can be found using:
+   *   el = dom.template('2.16.840.1.113883.10.20.22.2.17');
+   */
+  var template = function (templateId) {
+    var el = tagAttrVal(this.el, 'templateId', 'root', templateId);
+    if (!el) {
+      return emptyEl();
+    } else {
+      return wrapElement(el.parentNode);
+    }
+  };
+  
+  /*
+   * Search for the first occurrence of an element by tag name.
+   */
+  var tag = function (tag) {
+    var el = this.el.getElementsByTagName(tag)[0];
+    if (!el) {
+      return emptyEl();
+    } else {
+      return wrapElement(el);
+    }
+  };
+  
+  /*
+   * Search for all elements by tag name.
+   */
+  var elsByTag = function (tag) {
+    return wrapElement(this.el.getElementsByTagName(tag));
+  };
+  
+  /*
+   * Retrieve the element's attribute value. Example:
+   *   value = el.attr('displayName');
+   */
+  var attr = function (attr) {
+    if (!this.el) { return null; }
+    // Workaround a bug in jsdom https://github.com/tmpvar/jsdom/issues/651
+    attr = isNode ? attr.toLowerCase() : attr;
+    return this.el.getAttribute(attr);
+  };
+  
+  /*
+   * Retrieve the element's value. For example, if the element is:
+   *   <city>Madison</city>
+   * Use:
+   *   value = el.tag('city').val();
+   */
+  var val = function () {
+    if (!this.el) { return null; }
+    try {
+      return this.el.childNodes[0].nodeValue;
+    } catch (e) {
+      return null;
+    }
+  };
+  
+  /*
+   * Creates and returns an empty DOM element with tag name "empty":
+   *   <empty></empty>
+   */
+  var emptyEl = function () {
+    var el = doc.createElement('empty');
+    return wrapElement(el);
+  };
+  
+  /*
+   * Determines if the element is empty, i.e.:
+   *   <empty></empty>
+   * This element is created by function `emptyEL`.
+   */
+  var isEmpty = function () {
+    if (this.el.tagName.toLowerCase() == 'empty') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  // Public Methods
+  ///////////////////////////
+  
+  /*
+   * Cross-browser XML parsing supporting IE8+ and Node.js.
+   */
   var parseXML = function (data) {
-    // Must be a string
+    // XML data must be a string
     if (!data || typeof data !== "string") {
       console.log("BB Error: XML data is not a string");
       return null;
     }
     
-    var xml, tmp;
-
+    var xml;
+    
     // Node
     if (isNode) {
       xml = jsdom.jsdom(data, jsdom.level(1, "core"));
+      
     // Browser
     } else {
+      
       // Standard parser
       if (window.DOMParser) {
         parser = new DOMParser();
@@ -64,7 +177,7 @@ var Core = function () {
         }
       }
     }
-
+    
     if (!xml || !xml.documentElement || xml.getElementsByTagName("parsererror").length) {
       console.log("BB Error: Could not parse XML");
       return null;
@@ -73,83 +186,19 @@ var Core = function () {
     return wrapElement(xml);
   };
   
-  var wrapElement = function (el) {
-    // el is an array of elements
-    if (el.length) {
-      var els = [];
-      for (var i = 0; i < el.length; i++) {
-        els.push(ElementWrapper(el[i]));
-      }
-      return els;
-    
-    // el is a single element
-    } else {
-      return ElementWrapper(el);
-    }
-  };
-  
-  var emptyEl = function () {
-    var el = doc.createElement('empty');
-    return wrapElement(el);
-  };
-  
-  var tagAttrVal = function (el, tag, attr, value) {
-    el = el.getElementsByTagName(tag);
-    for (var i = 0; i < el.length; i++) {
-      // Workaround a bug in jsdom https://github.com/tmpvar/jsdom/issues/651
-      attr = isNode ? attr.toLowerCase() : attr;
-      if (el[i].getAttribute(attr) === value) {
-        return el[i];
-      }
-    }
-  };
-  
-  var template = function (templateId) {
-    var el = tagAttrVal(this.el, 'templateId', 'root', templateId);
-    if (!el) {
-      return emptyEl();
-    } else {
-      return wrapElement(el.parentNode);
-    }
-  };
-  
-  var tag = function (tag) {
-    var el = this.el.getElementsByTagName(tag)[0];
-    if (!el) {
-      return emptyEl();
-    } else {
-      return wrapElement(el);
-    }
-  };
-  
-  var elsByTag = function (tag) {
-    return wrapElement(this.el.getElementsByTagName(tag));
-  };
-  
-  var attr = function (attr) {
-    if (!this.el) { return null; }
-    // Workaround a bug in jsdom https://github.com/tmpvar/jsdom/issues/651
-    attr = isNode ? attr.toLowerCase() : attr;
-    return this.el.getAttribute(attr);
-  };
-  
-  var val = function () {
-    if (!this.el) { return null; }
-    try {
-      return this.el.childNodes[0].nodeValue;
-    } catch (e) {
-      return null;
-    }
-  };
-  
-  var isEmpty = function () {
-    if (this.el.tagName.toLowerCase() == 'empty') {
-      return true;
-    } else {
-      return false;
-    }
-  };
-  
+  /*
+   * Parses an HL7 date in String form and creates a new Date object.
+   * 
+   * TODO: CCDA dates can be in form:
+   *   <effectiveTime value="20130703094812"/>
+   * ...or:
+   *   <effectiveTime>
+   *     <low value="19630617120000"/>
+   *     <high value="20110207100000"/>
+   *   </effectiveTime>
+   * When latter, parseDate will not be given type `String`, but `null` and
+   * log the error "date is not a string".
+   */
   var parseDate = function (str) {
     if (!str || typeof str !== "string") {
       console.log("Error: date is not a string");
@@ -162,6 +211,9 @@ var Core = function () {
     return new Date(year, month, day);
   };
   
+  /*
+   * Removes all `null` properties from an object.
+   */
   var trim = function (o) {
     var y;
     for (var x in o) {
@@ -173,8 +225,27 @@ var Core = function () {
       if (y instanceof Object) y = trim(y);
     }
     return o;
+  };
+  
+  // Init
+  ///////////////////////////
+  
+  // Establish the root object, `window` in the browser, or `global` in Node.
+  var root = this,
+      jsdom = undefined,
+      isNode = false,
+      doc = root.document; // Will be `undefined` if we're in Node
+
+  // Check if we're in Node. If so, pull in `jsdom` so we can simulate the DOM.
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      isNode = true;
+      jsdom = require("jsdom");
+      doc = new (jsdom.level(1, "core").Document)();
+    }
   }
   
+  // Reveal public methods
   return {
     parseXML: parseXML,
     parseDate: parseDate,
