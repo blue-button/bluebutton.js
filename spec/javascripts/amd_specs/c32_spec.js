3273dde45d3e4a881683dafb51aa5f131ee3d6ee
Ravi Sankar <ravi.sankar@elationemr.com>
2015-01-07 13:03:08 -0800

    Adjustments to better handle a CCD from Greenway, which I believe is reprsentative of an older version of a C32 CCD from before the HITSP section IDs were mandated. This commit directs the parser to also look for sections by the HL7 ID if the HITSP ID can't be found and adds a test to confirm that works.

--- spec/javascripts/amd_specs/c32_spec.js
+++ spec/javascripts/amd_specs/c32_spec.js
@@ -7,5 +7,12 @@ define(['../../../build/bluebutton'], function(BlueButton) {
     // the tests are defined in helpers/shared_spec.js
     runJsonTests(expectedOutput, 'c32', bb);
 
+    // We're also going to test a slight variant of the file above,
+    // which uses only old HL7 CCD tags, instead of the HITSP section tags
+    // to make sure we're resilient to that
+    record = readFixtures('../fixtures/c32/HITSP_C32_with_HL7_IDs.xml');
+    bb = BlueButton(record);
+    runJsonTests(expectedOutput, 'c32', bb);
+
   });
 });
--- spec/javascripts/node_specs/c32_spec.js
+++ spec/javascripts/node_specs/c32_spec.js
@@ -54,4 +54,12 @@ describe('C32', function() {
   // the tests are defined in helpers/shared_spec.js
   runJsonTests(expectedOutput, 'c32', bb);
 
+  // We're also going to test a slight variant of the file above,
+  // which uses only old HL7 CCD tags, instead of the HITSP section tags
+  // to make sure we're resilient to that
+  record = fs.readFileSync(path.resolve(__dirname,
+    '../fixtures/c32/HITSP_C32_with_HL7_IDs.xml'), 'utf-8');
+  bb = BlueButton(record);
+  runJsonTests(expectedOutput, 'c32', bb);
+
 });
