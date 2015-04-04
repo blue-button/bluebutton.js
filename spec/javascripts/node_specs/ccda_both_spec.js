5c1eb06106299da52fddb6d9c4565e7d3f7d6baa
Ravi Sankar <ravi.sankar@elationemr.com>
2015-04-03 17:29:00 -0700

    Many fewer data losses in going from JSON -> CCDA -> JSON

--- spec/javascripts/node_specs/ccda_both_spec.js
+++ spec/javascripts/node_specs/ccda_both_spec.js
@@ -3,15 +3,10 @@ var fs = require('fs'),
     runJsonTests = require('../helpers/shared_spec').runJsonTests,
     BlueButton = require('../../../build/bluebutton');
 
-describe('Emerge JSON -> CCDA -> JSON', function() {
-  // start with what would usually be our "expectedJson"
-  var jsonInputStr = fs.readFileSync(path.resolve(__dirname,
-    '../fixtures/json/emerge_ccda_expected_output.json'), 'utf-8');
+var runPassThroughTest = function(jsonInputStr) {
+  // Takes some initial JSON (jsonInputStr)
 
-  // the codeSystemName in EMERGE files differs from our convention
-  jsonInputStr = jsonInputStr.replace(/SNOMED\-CT/g, 'SNOMED CT');
-
-  // generate a CCDA from it
+  // Generates a CCDA from it
   var template = fs.readFileSync(path.resolve(__dirname,
       '../../../lib/generators/ccda_template.ejs'), 'utf-8');
   var bb = BlueButton(jsonInputStr, {
@@ -20,37 +15,71 @@ describe('Emerge JSON -> CCDA -> JSON', function() {
     testingMode: true
   });
   var bbGeneratedXml = bb.data;
-  console.log(bbGeneratedXml);
 
-  // now parse the CCDA with BB again
+  // Then parses the CCDA with BB again
   bb = BlueButton(bbGeneratedXml);
 
-  // make sure we get back what we sent in, minus a whitelist of expected differences
+  // And makes sure we get back what we sent in
+  // (minus a whitelist of expected differences, in each of the tests below)
   runJsonTests(JSON.parse(jsonInputStr), 'ccda', bb);
+};
+
+describe('Emerge JSON -> CCDA -> JSON', function() {
+  // start with what would usually be our "expectedJson"
+  var jsonInputStr = fs.readFileSync(path.resolve(__dirname,
+    '../fixtures/json/emerge_ccda_expected_output.json'), 'utf-8');
+
+  // the codeSystemName in EMERGE files differs from our convention
+  jsonInputStr = jsonInputStr.replace(/SNOMED\-CT/g, 'SNOMED CT');
+
+  runPassThroughTest(jsonInputStr);
 
 });
 
-// describe('NIST JSON -> CCDA -> JSON', function() {
-//   // start with what would usually be our "expectedJson"
-//   var jsonInputStr = fs.readFileSync(path.resolve(__dirname,
-//     '../fixtures/json/nist_ccda_expected_output.json'), 'utf-8');
+describe('NIST JSON -> CCDA -> JSON', function() {
+  // start with what would usually be our "expectedJson"
+  var jsonInputStr = fs.readFileSync(path.resolve(__dirname,
+    '../fixtures/json/nist_ccda_expected_output.json'), 'utf-8');
 
-//   jsonInputStr = jsonInputStr.replace('GPARNT', 'GRFTH');
+  // The display name and code disagree in the source file.
+  // We're going to end up relying on the display name, so fix the code to fit.
+  jsonInputStr = jsonInputStr.replace('GPARNT', 'GRFTH');
+  // Some modifications will be easier with the actual JSON
+  var parsedJson = JSON.parse(jsonInputStr);
+  // We don't generate encounter locations, because that would require adding a location
+  // type code that we don't want to have to pay attention to (and the location is optional
+  // but the code is not optional once you have a location)
+  parsedJson.encounters.forEach(function(encounter) {
+    encounter.location = {
+      street: [],
+      city : null,
+      state : null,
+      zip : null,
+      country : null,
+      organization : null
+    };
+  });
+  // We could probably make this one work without new data if someone runs into issues...
+  parsedJson.procedures.forEach(function(procedure) {
+    procedure.performer = {
+      street: [],
+      city : null,
+      state : null,
+      zip : null,
+      country : null,
+      organization : null,
+      phone: null
+    };
+  });
+  // If we include a med performer, there is some other data that becomes necessary
+  parsedJson.medications.forEach(function(medication) {
+    medication.prescriber = {
+        organization: null,
+        person: null
+      };
+  });
+  jsonInputStr = JSON.stringify(parsedJson);
 
-//   // generate a CCDA from it
-//   var template = fs.readFileSync(path.resolve(__dirname,
-//       '../../../lib/generators/ccda_template.ejs'), 'utf-8');
-//   var bb = BlueButton(jsonInputStr, {
-//     template: template,
-//     generatorType: 'ccda',
-//     testingMode: true
-//   });
-//   var bbGeneratedXml = bb.data;
+  runPassThroughTest(jsonInputStr);
 
-//   // now parse the CCDA with BB again
-//   bb = BlueButton(bbGeneratedXml);
-
-//   // make sure we get back what we sent in, minus a whitelist of expected differences
-//   runJsonTests(JSON.parse(jsonInputStr), 'ccda', bb);
-
-// });
+});
